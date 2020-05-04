9e039d16877d640aa329ed876c66e63ab0352eea
Tuanhoang2302 <55321074+Tuanhoang2302@users.noreply.github.com>
2020-05-04 16:45:33 +0700

    reupdate testing

--- game1/spec/Scene2_TestTrue.test.js
+++ game1/spec/Scene2_TestTrue.test.js
@@ -13,13 +13,18 @@ describe('check question 1', () => {
     const browser = await puppeteer.launch({
       headless: false,
       slowMo: 80,
-      args: ['--window-size=3000, 3000'],
+      args: [
+        '--window-size=1920,1080',
+      ],
     });
     page = await browser.newPage();
     await page.goto('http://localhost:8080/');
 
-    await expect(page).toMatch('There are');
-    await expect(page).toMatch('blocks');
+    // await expect(page).toMatch('There are');
+    const ques = await page.$eval('#ques1', (el) => el.textContent);
+    expect(ques).toBe('There are ');
+    const wordEndQues = await page.$eval('#word_end_ques1', (el) => el.textContent);
+    expect(wordEndQues).toBe(' blocks');
   }, 10000);
 
   test('check existance of input', async () => {
@@ -33,21 +38,23 @@ describe('check question 1', () => {
     await page.type('input#input1', '2');
     await page.waitForSelector('#thought1', {
       visible: true,
-    }, 500);
+    }, 1000);
   }, 10000);
 
   test('display value of 10 in result', async () => {
     await page.type('input#input1', '1');
     await page.click('input#input2');
     await page.type('input#input2', '0');
-  }, 10000);
+  }, 2000);
 }, 10000);
 
 
 describe('check question 2', () => {
   test('check existance of text', async () => {
-    await expect(page).toMatch('There are');
-    await expect(page).toMatch('blocks');
+    const ques = await page.$eval('#ques2', (el) => el.textContent);
+    expect(ques).toBe('There are ');
+    const wordEndQues = await page.$eval('#word_end_ques2', (el) => el.textContent);
+    expect(wordEndQues).toBe(' blocks');
   }, 10000);
 
   test('check existance of input', async () => {
@@ -74,8 +81,10 @@ describe('check question 2', () => {
 
 describe('check question 3', () => {
   test('check existance of text', async () => {
-    await expect(page).toMatch('There are');
-    await expect(page).toMatch('blocks');
+    const ques = await page.$eval('#ques3', (el) => el.textContent);
+    expect(ques).toBe('There are ');
+    const wordEndQues = await page.$eval('#word_end_ques3', (el) => el.textContent);
+    expect(wordEndQues).toBe(' blocks');
   }, 10000);
 
   test('check existance of input', async () => {
--- game1/src/Config/config.js
+++ game1/src/Config/config.js
@@ -3,41 +3,41 @@ import FadePlugin from '../../node_modules/phaser3-rex-plugins/plugins/fade-plug
 
 // tạo khung cho game
 export default {
-  
+
   type: Phaser.AUTO,
   parent: 'phaser-example',
   width: 1080,
   height: 680,
-  backgroundColor: "0xffffff",
-   scale: {
+  backgroundColor: '0xffffff',
+  scale: {
 
-     //autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,     
-   },  
+    // autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+  },
 
   // cái này dùng để enable cái dom trong bootGame
   dom: {
-      createContainer: true
+    createContainer: true,
   },
 
   // cái này dùng để enable physics function
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
-      
-      debug: true
-    }
+
+      debug: true,
+    },
   },
 
   // cái này dùng để enable audio function
   audio: {
-    disableWebAudio: true
+    disableWebAudio: true,
   },
 
   plugins: {
     global: [{
-        key: 'rexFade',
-        plugin: FadePlugin,
-        start: true
-    }]
-  }
+      key: 'rexFade',
+      plugin: FadePlugin,
+      start: true,
+    }],
+  },
 };
